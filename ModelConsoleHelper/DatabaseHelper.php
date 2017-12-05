2c77d635ba9772d689b30c7d15c60cb96c89d99c
Liz <396635210@qq.com>
2017-12-05 17:33:11 +0800

    Changes to be committed: 	modified:   ModelConsoleHelper/DatabaseHelper.php 	modified:   ModelConsoleHelper/TableHelper.php     添加charset

--- ModelConsoleHelper/DatabaseHelper.php
+++ ModelConsoleHelper/DatabaseHelper.php
@@ -26,7 +26,8 @@ class DatabaseHelper extends AbstractHelper
 
     public function installPDO(){
         $db = $this->db;
-        $pdo = new \PDO("mysql:host=" . $db['host'],
+        $pdo = new \PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'].
+            ";charset=". $db['charset'].";collate=". $db['collate'],
             $db['user'], $db['pass'], ['port'=>$db['port']]);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
--- ModelConsoleHelper/TableHelper.php
+++ ModelConsoleHelper/TableHelper.php
@@ -31,7 +31,8 @@ class TableHelper extends AbstractHelper
 
     public function installPDO(){
         $db = $this->db;
-        $pdo = new \PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $pdo = new \PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'].
+            ";charset=". $db['charset'].";collate=". $db['collate'],
             $db['user'], $db['pass'], ['port'=>$db['port']]);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
